a6d0611f7bf100c36b97ce05f6632b1f56ecafdb
jaywcjlove <398188662@qq.com>
2021-10-05 00:53:00 +0800

    type: Fix rewrite type.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Plugin } from 'unified';
-import { Root, Element, ElementContent } from 'hast';
+import { Root, Element, RootContent } from 'hast';
 import { visit } from 'unist-util-visit';
 import { selectAll } from 'hast-util-select';
 import { Test } from 'unist-util-is';
 
 export type RehypeRewriteOptions = {
   selector?: string;
-  rewrite(node: ElementContent, index: number | null, parent: Root | Element | null): void;
+  rewrite(node: Root | RootContent, index: number | null, parent: Root | Element | null): void;
 }
 
 const remarkRewrite: Plugin<[RehypeRewriteOptions?], Root> = (options) => {
@@ -23,7 +23,7 @@ const remarkRewrite: Plugin<[RehypeRewriteOptions?], Root> = (options) => {
       return;
     }
 
-    visit(tree, (node: ElementContent, index, parent) => {
+    visit(tree, (node: Root | RootContent, index, parent) => {
       rewrite(node, index, parent);
     });
   }
